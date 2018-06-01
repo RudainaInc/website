4a5ab9a4e160411d8fde86cefef72ee6866a951f
Flavio B <barinas.flavio@gmail.com>
2018-06-01 19:36:52 -0400

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 
 const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
-
 const app = express();
 
 // Body parser middleware
@@ -30,8 +27,6 @@ require('./config/passport')(passport);
 
 // Use Routes
 app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
