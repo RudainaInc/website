520b3a6298c0db37d0c0ad5be5245f004eeb7d9f
Flavio B <barinas.flavio@gmail.com>
2018-06-11 21:31:43 -0400

    test

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -4,27 +4,56 @@ class About extends Component {
     render() {
         return (
             <div className="container">
-                <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img className="d-block w-100" src="https://upload.wikimedia.org/wikipedia/commons/0/02/Temp_plate_blue.svg" alt="First slide"/>
-                        </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="https://upload.wikimedia.org/wikipedia/commons/0/02/Temp_plate_blue.svg" alt="Second slide"/>
-                        </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="https://upload.wikimedia.org/wikipedia/commons/0/02/Temp_plate_blue.svg" alt="Third slide"/>
-                        </div>
-                    </div>
-                    <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Previous</span>
-                    </a>
-                    <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Next</span>
-                    </a>
-                </div>
+                
+                <h2>
+                    Our mission is to
+                </h2>
+
+                <p>
+                    …promote health by providing supportive health care to vulnerable or low
+                    income pregnant women. This is done by providing information and
+                    materials every trimester that will enable them to take action or adopt
+                    specific behaviours that will ensure the safe birth of the child.
+                </p>
+
+                <p>
+                    While on this program, women who experience pregnancy related
+                    complications specific to Cerebral Palsy, Infant Hypoxic-Ischemic
+                    Encephalopathy (HIE), Anemia, and Pre-Term Births can qualify to receive
+                    financial assistance that is to be used directly for doctor approved therapy
+                    treatments and healthcare modalities.
+                </p>
+
+                <h2>
+                    Out long term goal
+                </h2>
+
+                <p>
+                    …Provide life baskets every trimester to the over 350,000 persons who are
+                    expecting a baby this year, and to repeat it the following year, and so on…
+                </p>
+
+                <h2>
+                    Our Story
+                </h2>
+
+                <p>
+                    A message from the CEO
+                </p>
+
+                <p>
+                    I wanted to take a minute and explain the name of this foundation and the meaning
+                    that it signifies. Rudaina was the name of my cousin. She suffered from Infant
+                    Hypoxic Ischemic Encephalopathy (HIE) as a result of pregnancy related
+                    complications. The doctors gave her a life expectancy of just a few years. In 2017,
+                    she passed away at the age of 20. After she passed, I was inspired by the pain it
+                    brought me that I made it my mission to try and help other families that are going
+                    through something similar. The Rudaina Foundation will not just focus on HIE cases,
+                    it will develop and grow to help anyone that is going through something so emotional
+                    and painful. Whether that help comes from financial resources or through a
+                    foundation that can provide support, Rudaina will always be in my heart and will
+                    forever fuel my fire to help and contribute all my potential to someone in need.
+                </p>
             </div>
         );
     }
