bf09e1394452efa46a7e0e081d7697c9022f3ec2
Flavio B <barinas.flavio@gmail.com>
2018-06-11 21:34:45 -0400

    small fix

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -4,56 +4,60 @@ class About extends Component {
     render() {
         return (
             <div className="container">
+
+                <div className="jumbotron">
                 
-                <h2>
-                    Our mission is to
-                </h2>
+                    <h2>
+                        Our mission is to
+                    </h2>
 
-                <p>
-                    …promote health by providing supportive health care to vulnerable or low
-                    income pregnant women. This is done by providing information and
-                    materials every trimester that will enable them to take action or adopt
-                    specific behaviours that will ensure the safe birth of the child.
-                </p>
+                    <p>
+                        …promote health by providing supportive health care to vulnerable or low
+                        income pregnant women. This is done by providing information and
+                        materials every trimester that will enable them to take action or adopt
+                        specific behaviours that will ensure the safe birth of the child.
+                    </p>
 
-                <p>
-                    While on this program, women who experience pregnancy related
-                    complications specific to Cerebral Palsy, Infant Hypoxic-Ischemic
-                    Encephalopathy (HIE), Anemia, and Pre-Term Births can qualify to receive
-                    financial assistance that is to be used directly for doctor approved therapy
-                    treatments and healthcare modalities.
-                </p>
+                    <p>
+                        While on this program, women who experience pregnancy related
+                        complications specific to Cerebral Palsy, Infant Hypoxic-Ischemic
+                        Encephalopathy (HIE), Anemia, and Pre-Term Births can qualify to receive
+                        financial assistance that is to be used directly for doctor approved therapy
+                        treatments and healthcare modalities.
+                    </p>
 
-                <h2>
-                    Out long term goal
-                </h2>
+                    <h2>
+                        Out long term goal
+                    </h2>
 
-                <p>
-                    …Provide life baskets every trimester to the over 350,000 persons who are
-                    expecting a baby this year, and to repeat it the following year, and so on…
-                </p>
+                    <p>
+                        …Provide life baskets every trimester to the over 350,000 persons who are
+                        expecting a baby this year, and to repeat it the following year, and so on…
+                    </p>
 
-                <h2>
-                    Our Story
-                </h2>
+                    <h2>
+                        Our Story
+                    </h2>
 
-                <p>
-                    A message from the CEO
-                </p>
+                    <p>
+                        A message from the CEO
+                    </p>
 
-                <p>
-                    I wanted to take a minute and explain the name of this foundation and the meaning
-                    that it signifies. Rudaina was the name of my cousin. She suffered from Infant
-                    Hypoxic Ischemic Encephalopathy (HIE) as a result of pregnancy related
-                    complications. The doctors gave her a life expectancy of just a few years. In 2017,
-                    she passed away at the age of 20. After she passed, I was inspired by the pain it
-                    brought me that I made it my mission to try and help other families that are going
-                    through something similar. The Rudaina Foundation will not just focus on HIE cases,
-                    it will develop and grow to help anyone that is going through something so emotional
-                    and painful. Whether that help comes from financial resources or through a
-                    foundation that can provide support, Rudaina will always be in my heart and will
-                    forever fuel my fire to help and contribute all my potential to someone in need.
-                </p>
+                    <p>
+                        I wanted to take a minute and explain the name of this foundation and the meaning
+                        that it signifies. Rudaina was the name of my cousin. She suffered from Infant
+                        Hypoxic Ischemic Encephalopathy (HIE) as a result of pregnancy related
+                        complications. The doctors gave her a life expectancy of just a few years. In 2017,
+                        she passed away at the age of 20. After she passed, I was inspired by the pain it
+                        brought me that I made it my mission to try and help other families that are going
+                        through something similar. The Rudaina Foundation will not just focus on HIE cases,
+                        it will develop and grow to help anyone that is going through something so emotional
+                        and painful. Whether that help comes from financial resources or through a
+                        foundation that can provide support, Rudaina will always be in my heart and will
+                        forever fuel my fire to help and contribute all my potential to someone in need.
+                    </p>
+
+                </div>
             </div>
         );
     }
