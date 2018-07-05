efcbe7facb0608be0b22d944a4c53adfab6aef85
Pengliang Zhang(Beck) <35582395+Pengliangzhang@users.noreply.github.com>
2018-07-05 18:18:25 -0400

    test

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 //const User = require('../../models/User');
 
 const Benefactor = require('../../models/Benefactor');
+const Volunteer = require('../../models/Volunteer');
+
 const User = require('../../models/User');
 
 router.get('/getBenefactor', (req, res) =>{
@@ -27,6 +29,38 @@ router.post('/approveBenefactor', (req, res)=>{
 
     //console.log(bId);
     Benefactor.findOne({"_id":benefactorId})
+    .then(user=>{
+        console.log(user);
+        user.status=true;
+        user.save();
+    }).catch(
+        err => null
+    )
+
+
+    res.json({msg:"test"})
+
+})
+
+router.get('/getVolunteer', (req, res) =>{
+    const volunteer = [];
+
+    Volunteer.find()
+    .populate("user")
+    .then( users => {
+        for (i=0; i < users.length; ++i) {
+            volunteer.push(users[i]);
+        }
+        res.json(volunteer);
+    });
+})
+
+router.post('/approveVolunteer', (req, res)=>{
+    const volunteerId = req.body.ids;
+    //console.log(typeof benefactorId);
+
+    //console.log(bId);
+    Volunteer.findOne({"_id":volunteerId})
     .then(user=>{
         console.log(user);
         user.status=true;
