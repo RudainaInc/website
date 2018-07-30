9c56535a1d3a1889a75c759df46361fe6532585d
lcommonl <trevor.cheslock@canada.gc.ca>
2018-07-30 19:51:18 -0400

    image hover

--- client/src/UI/containers/Tabs/Home/index.tsx
+++ client/src/UI/containers/Tabs/Home/index.tsx
@@ -141,9 +141,9 @@ class Funding extends React.Component<Props, State> {
                                         
                                     <div className="basket-container">
                                         <div id="carousel" style={{transform: "rotateY("+this.state.currdeg+"deg)" }}>
-                                            <div className="basket-container-item a"><img src="img/1st.JPG" alt=""/></div>
-                                            <div className="basket-container-item b"><img src="img/2nd tre.JPG" alt=""/></div>
-                                            <div className="basket-container-item c"><img src="img/3rd trimester.JPG" alt=""/></div>
+                                            <div class="basket-container-item a"><img src="img/1st.JPG" alt=""/></div>
+                                            <div class="basket-container-item b"><img src="img/2nd tre.JPG" alt=""/></div>
+                                            <div class="basket-container-item c"><img src="img/3rd trimester.JPG" alt=""/></div>
                                         </div>
                                     </div>
 
