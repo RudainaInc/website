2196a2986ee4e54f69ae3cff79177018c0cfc9e0
f.barinas <barinas.flavio@gmail.com>
2018-07-31 19:33:10 -0400

    temp fr

---
+++ client/src/UI/containers/Tabs/About/components/OurStoryFr.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+const OurStoryFr = () => {
+
+    return ( 
+        <div>
+
+            <h2 className="news-section_name">
+                Our Stroy
+            </h2>
+            <hr className="style"/>
+
+            <p>
+                A message from the CEO
+            </p>
+
+            <p>
+               test
+            </p>
+
+        </div>
+    )
+}
+
+
+export default OurStoryFr;
--- client/src/UI/containers/Tabs/About/index.tsx
+++ client/src/UI/containers/Tabs/About/index.tsx
@@ -4,20 +4,34 @@ import TabSection from '../../TabSection';
 import LongTermGoal from "./components/LongTermGoal";
 import OurMission from './components/OurMission';
 import OurStory from './components/OurStory';
+import OurStoryFr from './components/OurStoryFr';
 
 import { GenericBody } from '../../../components';
 
 class About extends React.Component {
    public render() {
+
+        const isFr = true;
+
+        const content = isFr ? (
+            <GenericBody
+                labels={["Our Mission", "Long Term", "Our Stroy"]}
+                pages={[<OurMission key={0}/>, <LongTermGoal key={1}/>, <OurStoryFr key={2}/>]}
+                icons={["","",""]}
+            />
+        ) : (
+            <GenericBody
+                labels={["Our Mission", "Long Term", "Our Stroy"]}
+                pages={[<OurMission key={0}/>, <LongTermGoal key={1}/>, <OurStory key={2}/>]}
+                icons={["","",""]}
+            />
+        );
+        
         return (
             <TabSection>
                 <div className="container">
 
-                    <GenericBody
-                        labels={["Our Mission", "Long Term", "Our Stroy"]}
-                        pages={[<OurMission key={0}/>, <LongTermGoal key={1}/>, <OurStory key={2}/>]}
-                        icons={["","",""]}
-                    />
+                   { content }
 
                 </div>
             </TabSection>
