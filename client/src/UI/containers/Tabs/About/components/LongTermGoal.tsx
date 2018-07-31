606b94b54ea3f21c19d04484681ec2cbfda33e4a
lcommonl <trevor.cheslock@canada.gc.ca>
2018-07-31 18:44:05 -0400

    french

--- client/src/UI/containers/Tabs/About/components/LongTermGoal.tsx
+++ client/src/UI/containers/Tabs/About/components/LongTermGoal.tsx
@@ -3,16 +3,25 @@ import * as React from 'react';
 const LongTermGoal = () => {
 
     return ( 
-        <div>
+        
+            <div>
 
-            <h2 className="news-section_name">
-                Out long term goal
+            <h2 lang="en" className="news-section_name">
+                Our long term goal
+            </h2>
+
+            <h2 lang="fr" className="news-section_name">
+                French Header Here
             </h2>
             <hr className="style"/>
 
-            <p>
+            <p lang="en">
                 Provide life baskets every trimester to the over 350,000 persons who are
                 expecting a baby this year, and to repeat it the following year, and so on
+            </p>
+
+            <p lang="fr">
+                blah blah french text
             </p>
 
         </div>
