725436c4e3536688ddca7d8b9c20f95f63ab77e9
Flavio B <barinas.flavio@gmail.com>
2018-08-09 17:39:20 -0400

    push:

--- sever/server.js
+++ sever/server.js
@@ -41,6 +41,11 @@ app.use('/api/admin', admin);
 // Use Build
 app.use(express.static('build'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
