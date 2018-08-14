f346b4553a156119d37508dfa68972ea6fa640a2
lcommonl <trevor.cheslock@canada.gc.ca>
2018-08-13 21:07:06 -0400

    revert

--- client/src/UI/containers/Tabs/Example/index.tsx
+++ client/src/UI/containers/Tabs/Example/index.tsx
@@ -10,68 +10,10 @@ type Props = any;
 type State = any;
 
 const exp1 = (
-        <form id="contact-form" method="post" action="">
+    <div>
+        hello
 
-            <div className="controls">
-
-                <div className="row">
-                    <div className="col-md-6">
-                        <TextFieldGroup
-                            type="text"
-                            name="fname"
-                            placeholder="First Name"
-                            lable="First Name*"
-                            />
-                    </div>
-                    <div className="col-md-6">
-                        <TextFieldGroup
-                            type="text"
-                            name="lname"
-                            placeholder="Last Name"
-                            lable="Last Name*"
-                            />
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-md-6">
-                        <TextFieldGroup
-                            type="email"
-                            name="email"
-                            placeholder="Email Address"
-                            lable="Email*"
-                            />
-                    </div>
-                    <div className="col-md-6">
-                        <TextFieldGroup
-                            type="tel"
-                            name="phone"
-                            placeholder="Please enter your phone"
-                            lable="Phone(Optional)"
-                            />
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="form-group">
-
-                            <TextAreaGroup
-                                name="message"
-                                className="form-control"
-                                placeholder="Message for me *"
-                                label="Message *"
-                                />
-
-                        </div>
-                    </div>
-                    <div className="col-md-12">
-                        <input type="submit" className="big-font btn btn-info btn-block mt-4 "/>
-                    </div>
-                </div>
-            </div>
-
-        </form> 
+    </div>
 )
 
 const exp2 = (
