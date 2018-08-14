6aaf460c12ac03e15c5d5e883f3d6f56fb40eb9a
lcommonl <trevor.cheslock@canada.gc.ca>
2018-08-13 20:35:11 -0400

    form for exmple

--- client/src/UI/containers/Tabs/Example/index.tsx
+++ client/src/UI/containers/Tabs/Example/index.tsx
@@ -9,7 +9,83 @@ type State = any;
 
 const exp1 = (
     <div>
-        hello
+        <form id="contact-form" method="post" action="">
+
+            <div className="controls">
+
+                <div className="row">
+                    <div className="col-md-6">
+                        <TextFieldGroup
+                            type="text"
+                            name="fname"
+                            placeholder="First Name"
+                            value={this.state.fname}
+                            onChange={this.onChange}
+                            error={errors.fname}
+                            lable="First Name*"
+                            />
+                    </div>
+                    <div className="col-md-6">
+                        <TextFieldGroup
+                            type="text"
+                            name="lname"
+                            placeholder="Last Name"
+                            value={this.state.lname}
+                            onChange={this.onChange}
+                            error={errors.lname}
+                            lable="Last Name*"
+                            />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-6">
+                        <TextFieldGroup
+                            type="email"
+                            name="email"
+                            placeholder="Email Address"
+                            value={this.state.email}
+                            onChange={this.onChange}
+                            error={errors.email}
+                            lable="Email*"
+                            />
+                    </div>
+                    <div className="col-md-6">
+                        <TextFieldGroup
+                            type="tel"
+                            name="phone"
+                            placeholder="Please enter your phone"
+                            value={this.state.phone}
+                            onChange={this.onChange}
+                            error={errors.phone}
+                            lable="Phone(Optional)"
+                            />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="form-group">
+
+                            <TextAreaGroup
+                                name="message"
+                                className="form-control"
+                                placeholder="Message for me *"
+                                value={this.state.message}
+                                onChange={this.onChange}
+
+                                label="Message *"
+                                />
+
+                        </div>
+                    </div>
+                    <div className="col-md-12">
+                        <input type="submit" className="big-font btn btn-info btn-block mt-4 "/>
+                    </div>
+                </div>
+            </div>
+
+        </form> 
     </div>
 )
 
