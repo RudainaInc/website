f3266e6f344125f7f89ff406f2e68cc87401a827
lcommonl <trevor.cheslock@canada.gc.ca>
2018-08-15 18:42:46 -0400

    form for example

--- client/src/UI/containers/Tabs/Example/index.tsx
+++ client/src/UI/containers/Tabs/Example/index.tsx
@@ -9,10 +9,147 @@ type Props = any;
 type State = any;
 
 const exp1 = (
-    <div>
-        hello
+    <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+        <style type="text/css">
+            .form-style-10{
+                width:450px;
+            padding: 30px;
+            margin: 40px auto;
+            background: #FFF;
+            border-radius: 10px;
+            -webkit-border-radius: 10px;
+            -moz-border-radius: 10px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+            -moz-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+            -webkit-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+            }
+            .form-style-10.inner-wrap{
+                padding: 30px;
+            background: #F8F8F8;
+            border-radius: 6px;
+            margin-bottom: 15px;
+            }
+            .form-style-10 h1{
+                background: #A60002;
+            padding: 20px 30px 15px 30px;
+            margin: -30px -30px 30px -30px;
+            border-radius: 10px 10px 0 0;
+            -webkit-border-radius: 10px 10px 0 0;
+            -moz-border-radius: 10px 10px 0 0;
+            color: #fff;
+            text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.12);
+            font: normal 30px 'Bitter', serif;
+            -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+            -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+            box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+            border: 1px solid #A60002;
+            }
+            .form-style-10 h1 > span{
+                display: block;
+            margin-top: 2px;
+            font: 13px Arial, Helvetica, sans-serif;
+            }
+            .form-style-10 label{
+                display: block;
+            font: 13px Arial, Helvetica, sans-serif;
+            color: #888;
+            margin-bottom: 15px;
+            }
+            .form-style-10 input[type ="text"],
+            .form-style-10 input[type ="date"],
+            .form-style-10 input[type ="datetime"],
+            .form-style-10 input[type ="email"],
+            .form-style-10 input[type ="number"],
+            .form-style-10 input[type ="search"],
+            .form-style-10 input[type ="time"],
+            .form-style-10 input[type ="url"],
+            .form-style-10 input[type ="password"],
+            .form-style-10 input[type ="text2"],
+            .form-style-10 textarea,
+            .form-style-10 select {
+                display: block;
+            box-sizing: border-box;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            width: 100%;
+            padding: 8px;
+            border-radius: 6px;
+            -webkit-border-radius: 6px;
+            -moz-border-radius: 6px;
+            border: 2px solid #fff;
+            box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
+            -moz-box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
+            -webkit-box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
+            }
 
-    </div>
+            .form-style-10.section{
+                font: normal 20px 'Bitter', serif;
+            color: #A60002;
+            margin-bottom: 5px;
+            }
+            .form-style-10.section span {
+                background: #A60002;
+            padding: 5px 10px 5px 10px;
+            position: absolute;
+            border-radius: 50%;
+            -webkit-border-radius: 50%;
+            -moz-border-radius: 50%;
+            border: 4px solid #fff;
+            font-size: 14px;
+            margin-left: -45px;
+            color: #fff;
+            margin-top: -3px;
+            }
+            .form-style-10 input[type ="button"],
+            .form-style-10 input[type ="submit"]{
+                background: #A60002;
+            padding: 8px 20px 8px 20px;
+            border-radius: 5px;
+            -webkit-border-radius: 5px;
+            -moz-border-radius: 5px;
+            color: #fff;
+            text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.12);
+            font: normal 30px 'Bitter', serif;
+            -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+            -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+            box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+            border: 1px solid #257C9E;
+            font-size: 15px;
+            }
+            .form-style-10 input[type ="button"]: hover,
+            .form-style-10 input[type ="submit"]: hover{
+                background: #A60002;
+            -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+            -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+            box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+            }
+            .form-style-10.privacy-policy{
+                float: right;
+            width: 250px;
+            font: 12px Arial, Helvetica, sans-serif;
+            color: #4D4D4D;
+            margin-top: 10px;
+            text-align: right;
+            }
+        </style>><div class="form-style-10">
+            <h1>Contact Us</span></h1>
+        <form>
+            <div class="section"><span>1</span>Contact Information</div>
+            <div class="inner-wrap">
+                <label>Your Full Name <input type="text" name="field1" /></label>
+                <label>Email Address <input type="email" name="field3" /></label>
+                <label>Phone Number <input type="number" name="field3" /></label>
+            </div>
+
+            <div class="section"><span>2</span>Message</div>
+            <div class="inner-wrap">
+                <textarea name="field3" placeholder="Type your Message"></textarea>
+            </div>
+            <div class="button-section">
+                <input type="submit" name="Sign Up" />
+            </div>
+        </form>
+</div>
 )
 
 const exp2 = (
