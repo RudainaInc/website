2ab062b409564097a78df242f34f944a303f647d
lcommonl <trevor.cheslock@canada.gc.ca>
2018-08-15 20:01:05 -0400

    css fix

--- client/src/UI/containers/Tabs/Example/index.tsx
+++ client/src/UI/containers/Tabs/Example/index.tsx
@@ -14,14 +14,14 @@ const exp1 = (
        <div className="form-style-10">
             <h1>Contact Us</h1>
         <form>
-            <div className="section"><span>1</span>Contact Information</div>
+            <div className="section">Contact Information</div>
             <div className="inner-wrap">
                 <label>Your Full Name <input type="text" name="field1" /></label>
                 <label>Email Address <input type="email" name="field3" /></label>
                 <label>Phone Number <input type="number" name="field3" /></label>
             </div>
 
-            <div className="section"><span>2</span>Message</div>
+            <div className="section">Message</div>
             <div className="inner-wrap">
                 <textarea name="field4" placeholder="Type your Message" />
             </div>
